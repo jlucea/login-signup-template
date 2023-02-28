4790cda8e298940211889953510191f9d59ff5e6
Jaime Lucea <jlucea.ios@gmail.com>
2023-02-28 10:05:49 +0100

    Initial Commit

---
+++ LoginTemplate/LoginTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTemplateApp.swift
+//  LoginTemplate
+//
+//  Created by Jaime Lucea on 28/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
